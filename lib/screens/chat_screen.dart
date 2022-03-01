1f09c79f4fa1533308fe5dcd6179c0aa439f5e4d
minhho <minh.ho89@gmail.com>
2022-03-01 21:56:17 +0700

    chat screen update messages stream

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,10 +14,12 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
+  final Stream<QuerySnapshot> _messagesStream =
+      FirebaseFirestore.instance.collection('messages').snapshots();
   String _message = '';
 
   Future<void> _addMessage(String sender, String message) async {
-    _firestore.collection('messages').add({'sender' : sender, 'text': message});
+    _firestore.collection('messages').add({'sender': sender, 'text': message});
   }
 
   String _getCurrentUser() {
@@ -26,6 +28,20 @@ class _ChatScreenState extends State<ChatScreen> {
     return user?.email ?? '';
   }
 
+  void _getMessages() async {
+    final messages = await _firestore.collection('messages').get();
+    for (var message in messages.docs) {
+      print(message.data());
+    }
+  }
+
+  // void _messagesStream() async {
+  //   await for (var snapshot in _firestore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +64,26 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _messagesStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text('Loading');
+                }
+
+                return Column(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Text('${data['sender']} : ${data['text']}');
+                  }).toList(),
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -63,7 +99,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
-                     _addMessage(_getCurrentUser(), _message);
+                      _addMessage(_getCurrentUser(), _message);
                     },
                     child: Text(
                       'Send',
